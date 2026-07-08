7d18ecb2128a0adb7c7cd7b0cd503f3bf351920c
CreativeCookieConnections <creativecookieconnections@gmail.com>
2026-07-08 17:50:56 -0500

    implemented the API to create an inventory item

---
+++ ims-server/src/routes/itemInventory/index.js
@@ -0,0 +1,42 @@
+/**
+ * Author: Aisha Keller
+ * Date: 07/08/2026
+ * File: index.js
+ * Description: API to create an inventory item.
+ */
+
+
+const express = require('express');
+const Ajv = require('ajv');
+const createError = require('http-errors');
+const router = express.Router();
+const { itemInventory } = require('../../models/itemInventory');
+const { addItemInventory } = require('../../schemas/itemInventory');
+
+const ajv = new Ajv();
+const validateAddItemInventory = ajv.compile(addItemInventory);
+
+// POST request to create an inventory item Sprint 1 Student A Week 6
+router.post('/', async (req, res, next) => {
+    try {
+        const valid = validateAddItemInventory(req.body);
+
+        if (!valid) {
+            return next(createError(400, ajv.errorsText(validateAddItemInventory.errors)));
+        }
+
+        const newItemInventory = new itemInventory(req.body);
+        await newItemInventory.save();
+
+        res.end({
+            message: 'Inventory item created successfully',
+            itemInventoryId: newItemInventory._id
+        })
+    } catch (err) {
+        console.error(`Error while creating garden: ${err}`);
+        next(err);
+    }
+});
+
+module.exports = router;
+
