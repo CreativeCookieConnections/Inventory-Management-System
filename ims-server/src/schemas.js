61d1f792aa9f157ada028ed20d1f1aa0cba87f53
CreativeCookieConnections <creativecookieconnections@gmail.com>
2026-07-08 18:38:39 -0500

    added the schemas.js for creating an inventory item

--- ims-server/src/schemas.js
+++ ims-server/src/schemas.js
@@ -9,9 +9,18 @@
 const addInventoryItemSchema = {
     type: 'object',
     properties: {
-        name: { type: 'string' }
+        itemInventoryId: { type: 'number', minimum: 1, maximum: 100 },
+       categoryId: { type: 'number', minimum: 1, maximum: 100 },
+       supplierId: { type: 'number', minimum: 1, maximum: 100 },
+       name: { type: 'string' },
+       description: { type: 'string', maxLength: 500 },
+       quantity: { type: 'number', minimum: 0 },
+       price: { type: 'number', minimum: 0 },
+       dateCreated: { type: 'string', pattern: '^(\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}\\.\\d{3}Z)?$' }, // YYYY-MM-DD format
+       dateUpdated: { type: 'string', pattern: '^(\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}\\.\\d{3}Z)?$' } // YYYY-MM-DD format
+
     },
-    required: ['name']
+    required: ['itemInventoryId', 'categoryId', 'supplierId', 'name', 'description', 'quantity', 'price', 'dateCreated', 'dateUpdated']
 };
 
 module.exports = {
