07fbf7c11fa6c3d9059113d0530878a71d9b5229
CreativeCookieConnections <creativecookieconnections@gmail.com>
2026-07-08 23:00:17 -0500

    applied changes to the app component spec ts file

--- ims-client/src/app/app.component.spec.ts
+++ ims-client/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'ims-client' title`, () => {
+  it(`should have the 'ets-client' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ims-client');
+    expect(app.title).toEqual('ets-client');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ims-client');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Welcome to the MEAN Stack Starter Project');
   });
 });
