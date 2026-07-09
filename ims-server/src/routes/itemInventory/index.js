a49cf66c8a32ca33992759ba315a32cecc85469a
CreativeCookieConnections <creativecookieconnections@gmail.com>
2026-07-08 20:38:46 -0500

    redoing the api implementation

--- ims-server/src/routes/itemInventory/index.js
+++ ims-server/src/routes/itemInventory/index.js
@@ -6,37 +6,4 @@
  */
 
 
-const express = require('express');
-const Ajv = require('ajv');
-const createError = require('http-errors');
-const router = express.Router();
-const { itemInventory } = require('../../models/itemInventory');
-const { addItemInventory } = require('../../schemas');
-
-const ajv = new Ajv();
-const validateAddItemInventory = ajv.compile(addItemInventorySchema);
-
-// POST request to create an inventory item Sprint 1 Student A Week 6
-router.post('/', async (req, res, next) => {
-    try {
-        const valid = validateAddItemInventory(req.body);
-
-        if (!valid) {
-            return next(createError(400, ajv.errorsText(validateAddItemInventory.errors)));
-        }
-
-        const newItemInventory = new itemInventory(req.body);
-        await newItemInventory.save();
-
-        res.end({
-            message: 'Inventory item created successfully',
-            itemInventoryId: newItemInventory.inventoryItemId
-        })
-    } catch (err) {
-        console.error(`Error while creating garden: ${err}`);
-        next(err);
-    }
-});
-
-module.exports = router;
 
