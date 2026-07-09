cecf540d2cb6e47674e55606f799e2e528052e3e
CreativeCookieConnections <creativecookieconnections@gmail.com>
2026-07-08 19:26:47 -0500

    applied the app.routes.ts for the home component and the add inventory item component

--- ims-client/src/app/app.routes.ts
+++ ims-client/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { InventoryItemAddComponent } from './inventory-add/inventory-item-add.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+    },
+    {
+        path: 'inventory/add',
+        component: InventoryItemAddComponent,
+    }
+];
+
+
+
--- ims-client/src/app/inventory-item-add/inventory-item-add.component.spec.ts
+++ ims-client/src/app/inventory-item-add/inventory-item-add.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { InventoryItemAddComponent } from './inventoryItem-add.component';
+import { InventoryItemAddComponent } from './inventory-item-add.component';
 
 describe('InventoryItemAddComponent', () => {
   let component: InventoryItemAddComponent;
