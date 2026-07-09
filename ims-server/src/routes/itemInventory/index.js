26c390742f420bf7ab438b9217ce33d261350e5f
CreativeCookieConnections <creativecookieconnections@gmail.com>
2026-07-08 21:17:44 -0500

    updated the API

--- ims-server/src/routes/itemInventory/index.js
+++ ims-server/src/routes/itemInventory/index.js
@@ -5,32 +5,47 @@
  * Description: API to create an inventory item.
  */
 
+const express = require('express');
+const Ajv = require('ajv');
+const createError = require('http-errors');
+const router = express.Router();
+
+const { InventoryItem } = require('../../models/inventoryItem');
+const { addInventoryItemSchema } = require('../../schemas/inventoryItemSchema');
+
+const ajv = new Ajv();
+const validateAddInventoryItem = ajv.compile(addInventoryItemSchema);
+
 // POST request to add a new item to the inventory
-router.post('/', async (req, res) => {
+router.post('/', async (req, res, next) => {
     try {
-        const valid = validateAddItem(req.body);
+        const valid = validateAddInventoryItem(req.body);
 
         if (!valid) {
-            return next(createError(400, ajv.errorsText(validateAddItem.errors)));
-    }
+            return next(createError(400, ajv.errorsText(validateAddInventoryItem.errors)));
+        }
 
-    const payload = {
-        ...req.body,
-        itemId: req.params.itemId
-    }
+    const newInventoryItem = new InventoryItem({
+        categoryId: req.body.categoryId,
+        supplierId: req.body.supplierId,
+        name: req.body.name,
+        description: req.body.description,
+        quantity: req.body.quantity,
+        price: req.body.price
+    });
 
-    const item = new Item(payload);
-    await item.save();
+    const savedItem = await newInventoryItem.save();
 
-    res.send({
-        message: 'Item added to inventory successfully',
-        id: item._id
+    res.status(201).json({
+        message: 'Inventory item created successfully',
+        item: savedItem
     });
     } catch (err) {
-        console.error(`Error while creating item: ${err}`);
         next(err);
-}
+    }
 });
+
+module.exports = router;
 
 
 
